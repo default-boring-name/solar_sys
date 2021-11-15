2ff3b4050b2bc6ad95ce389eada61738010872ae
default-boring-name <vasiakin.mm@phystech.edu>
2021-11-15 10:07:38 +0300

    Начал писать solar_model.py 1) Добавил класс Model физической модели,    симмулирующей движение космических объектов

---
+++ prototype-version/solar_model.py
@@ -0,0 +1,52 @@
+import copy
+
+class Model:
+	'''
+	Класс физической модели
+	'''
+	
+	def __init__(self):
+		'''
+		Функция, иницализирующая модель
+		'''
+		self.space_objs = []
+		
+	def load(self, objects):
+		'''
+		Функция, загружающая объекты из переданного массива
+		:param objects: массив с объектами, которые будут добавлены в проект
+		'''
+		self.space_objs = objects
+	
+	def update(self, dt):
+		'''
+		Функция, обновляющая модель в соответствии с dt
+		:param dt: изменение времени
+		'''
+		for obj in self.space_objs:
+			obj.calculate_force(self.space_objs)
+		
+		for obj in self.space_objs:
+			obj.move
+	
+	def dump(self):
+		'''
+		Функция, возвращающая последнее состояние модели
+		'''
+		
+		return copy.deepcopy(self.space_objs)
+
+	def get_max_distance:
+		'''
+		Функция, возвращающая максимальное расстояние между
+		объектами
+		'''
+
+		distance = 0
+
+		for obj_1 in self.space_objs:
+			for obj_2 in self.space_objs:
+				new_distance = ((obj_1.x - obj_2.x) ** 2 + (obj_1.y - obj_2.y) ** 2)
+				distance = max(new_distance, distance)
+	
+		return distance
